6bf84c161f859305fd1d92d3455cce262c2398d0
Vamsivarma116 <85391702+Vamsivarma116@users.noreply.github.com>
2024-12-29 11:25:34 +0000

    basic datatypes completed

---
+++ 01.basic/02_dataTypes.js
@@ -0,0 +1,38 @@
+"use strict"; // treats all code as newer version
+
+//alert(3+3)
+//  ^
+//  |
+// we are using it in nodejs not in browser it has other syntax here
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("vamsi")
+
+let name = "vamsi"
+
+let age = 35
+
+let isLoggedIn = false
+
+// number => 2^53
+// bigint
+// string => ""
+//boolean => true/false
+//null => standlone value
+//undefined => when value is not assigned
+//symbol => unique
+
+
+// object
+
+console.log(typeof null);
+
+console.log(typeof undefined);
+
+
+
+
+
